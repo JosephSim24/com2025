9be4bdce3a9cb54412a575f1517a01a7c9d61b71
Joe <js02831@surrey.ac.uk>
2023-11-27 21:29:27 +0000

    Progressing with modelling albums, songs and users

--- app_album_viewer/models.py
+++ app_album_viewer/models.py
@@ -11,16 +11,40 @@ def validate_format(value):
         raise ValidationError(_("InvalidFormat"))
 
 
-def h():
+def validate_release_date(value):
+    if value.year > datetime.date.today().year + 3:
+        raise ValidationError(_("ReleaseDateTooFar"))
+    elif value.year > datetime.date.today().year + 1:
+        value.month = 1
 
 
 class Album(models.Model):
-    #cover
+    class Comments(models.Model):
+        user_display_name = "" #TODO
+        message = "" #TODO
+
+    cover = models.ImageField(default="/media/default-cover.png",
+                            upload_to="media")
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
     artist = models.CharField(max_length=60)
     price = models.DecimalField()
     format = models.CharField(validators=[validate_format])
-    #release_date
+    release_date = models.DateField(validators=[validate_release_date])
     #assocSongs
-    #comments
+    comments = models.Aggregate(Comments.user_display_name
+                            + Comments.message) #TODO Double check this works
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=200)
+    runtime = models.IntegerField()
+    album = models.Aggregate() #TODO
+
+
+class User(models.Model):
+    email = models.CharField(max_length=250)
+    username = models.CharField(max_length=100)
+    password = "password"
+    user_display_name = models.CharField(max_length=100)
+    message = models.CharField(max_length=1000)
