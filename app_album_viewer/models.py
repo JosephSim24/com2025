5a6e3161ddb0b5f8593a144a8d4ce18a8e9d9be1
Joe <js02831@surrey.ac.uk>
2023-11-27 20:20:37 +0000

    Another Test

--- app_album_viewer/models.py
+++ app_album_viewer/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
+import datetime
 
-# Create your models here.
+
+valid_formats = ["Digital download", "CD", "Vinyl"]
+
+def validate_format(value):
+    if not valid_formats.__contains__(value):
+        raise ValidationError(_("InvalidFormat"))
+
+
+def 
+
+
+class Album(models.Model):
+    #cover
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    artist = models.CharField(max_length=60)
+    price = models.DecimalField()
+    format = models.CharField(validators=[validate_format])
+    #release_date
+    #assocSongs
+    #comments
