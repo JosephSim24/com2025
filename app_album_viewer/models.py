0bc42d39a82e647444f78d57cdce174a17b6dc51
Joe <js02831@surrey.ac.uk>
2023-11-28 16:31:37 +0000

    Further progressing with the models.

--- app_album_viewer/models.py
+++ app_album_viewer/models.py
@@ -4,47 +4,54 @@ from django.utils.translation import gettext as _
 import datetime
 
 
-valid_formats = ["Digital download", "CD", "Vinyl"]
-
-def validate_format(value):
-    if not valid_formats.__contains__(value):
-        raise ValidationError(_("InvalidFormat"))
-
-
 def validate_release_date(value):
     if value.year > datetime.date.today().year + 3:
         raise ValidationError(_("ReleaseDateTooFar"))
     elif value.year > datetime.date.today().year + 1:
-        value.month = 1
+        Album.release_date = [value.year + "-" + 1
+                            + "-" + value.day]
 
 
 class Album(models.Model):
-    class Comments(models.Model):
-        user_display_name = "" #TODO
-        message = "" #TODO
+    class Comment(models.Model):
+        user_display_name = models.CharField(max_length=50)
+        message = models.TextField()
 
-    cover = models.ImageField(default="/media/default-cover.png",
+    class Format(models.TextChoices):
+        DD = "DD", _("DigitalDownload")
+        CD = "CD", _("CD")
+        VINYL = "VINYL", _("Vinyl")
+
+    cover = models.ImageField(default="media/default-cover.png",
                             upload_to="media")
     title = models.CharField(max_length=200)
-    description = models.CharField(max_length=1000)
+    description = models.TextField(null=True,
+                                blank=True)
     artist = models.CharField(max_length=60)
-    price = models.DecimalField()
-    format = models.CharField(validators=[validate_format])
-    release_date = models.DateField(validators=[validate_release_date])
+    price = models.DecimalField(default=0.00,
+                                max_digits=5,
+                                decimal_places=2)
+    format = models.CharField(max_length=5,
+                            choices=Format.choices, 
+                            default=Format.DD)
+    release_date = models.DateField(null=True,
+                        validators=[validate_release_date])
     #assocSongs
-    comments = models.Aggregate(Comments.user_display_name
-                            + Comments.message) #TODO Double check this works
+    comments = models.ForeignKey(Comment, on_delete=models.CASCADE,
+                            null=True, blank=True)
 
+    def __str__(self):
+        return self.title
 
 class Song(models.Model):
     title = models.CharField(max_length=200)
     runtime = models.IntegerField()
-    album = models.Aggregate() #TODO
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
 
 
-class User(models.Model):
-    email = models.CharField(max_length=250)
-    username = models.CharField(max_length=100)
-    password = "password"
-    user_display_name = models.CharField(max_length=100)
-    message = models.CharField(max_length=1000)
+#class User(models.Model):
+    #email = models.CharField(max_length=250)
+    #username = models.CharField(max_length=100)
+    #password = "password"
+    #user_display_name = models.CharField(max_length=100)
+    #message = models.CharField(max_length=1000)
