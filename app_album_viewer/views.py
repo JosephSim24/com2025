bb86c790e47bbacce77c833243e9a0e2fabfc62e
Joe <js02831@surrey.ac.uk>
2023-12-05 03:56:28 +0000

    Finishing up sites and views

--- app_album_viewer/views.py
+++ app_album_viewer/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from .models import Album
+from .models import Album, Comment, Song
 from django.shortcuts import get_object_or_404
 from django.db.models import Count
+from datetime import date
 
 def index_albums(request):
     #retreive all albums from the db and annotate each with a comment count
@@ -15,8 +16,15 @@ def index_albums(request):
     return render(request, 'app_album_viewer/index.html', context)
 
 def show_album(request, album_id):
+    #retreive all albums and its respective comments and songs from the db
     album = get_object_or_404(Album, pk=album_id)
-    context = {'album': album}
+    comments = Comment.objects.filter(album=album)
+    songs = Song.objects.filter(albums=album)
+
+    #create a dictionary to store the data
+    context = {'album': album, 'comments': comments, 'songs': songs}
+
+    #render the page
     return render(request, 'app_album_viewer/show.html', context)
 
 def new_album(request):
@@ -31,7 +39,7 @@ def new_album(request):
         # create a form instance and populate it with data from the request:
         album = Album(title=title, artist=artist, price=price, 
             cover=cover, description=description, format=format,
-            release_date=release_date)
+            release_date=date(release_date))
         album.save()
 
         return HttpResponseRedirect('/app_album_viewer/')
