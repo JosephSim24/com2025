302eeaeed4f148d7f9f5ed8b77b2db8755c76584
Joe <js02831@surrey.ac.uk>
2023-12-06 15:58:59 +0000

    Added tests

--- app_album_viewer/tests.py
+++ app_album_viewer/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Album
 
 
 class AlbumsIndexPageTests(TestCase):
@@ -12,3 +14,18 @@ class AlbumsIndexPageTests(TestCase):
     def test_album_page(self):
         response = self.client.get('/albums/')
         self.assertEqual(response.status_code, 200)
+
+
+class AlbumModelTest(TestCase):
+    def test_release_date_month(self):
+        # Create an album with a release date more than 1 year away
+        future_release_date = timezone.now().date() + timezone.timedelta(days=400)
+        album = Album.objects.create(title='Test Album', release_date=future_release_date)
+
+        # Refresh the album instance from the database to get the updated release_date
+        album.refresh_from_db()
+
+        # Check if the month of the release_date is 1
+        self.assertEqual(album.release_date.month, 1)
+
+
